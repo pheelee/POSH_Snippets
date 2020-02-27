464f3b2dab185f330038f265483ce76ac4b88ec1
pheelee <pri@irbe.ch>
2020-02-27 15:05:51 +0100

    make one liner

--- TcpPortCon.ps1
+++ TcpPortCon.ps1
@@ -1,2 +1,1 @@
-Get-NetTCPConnection -LocalPort 8039,8040 -State Established | Select -Unique -Property RemoteAddress, LocalPort | 
-Select -Property @{L="Hostname";E={[System.Net.Dns]::GetHostByAddress($_.RemoteAddress).HostName}}, LocalPort
+Get-NetTCPConnection -LocalPort 8039,8040 -State Established | Select -Unique -Property RemoteAddress, LocalPort | Select -Property @{L="Hostname";E={[System.Net.Dns]::GetHostByAddress($_.RemoteAddress).HostName}}, LocalPort
