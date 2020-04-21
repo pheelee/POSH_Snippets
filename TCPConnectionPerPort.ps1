6ddc6731e20d21bd19f6567a42b5703942fde0c3
Philipp <2619327+pheelee@users.noreply.github.com>
2020-04-21 14:39:16 +0200

    Update TCPConnectionPerPort.ps1

--- TCPConnectionPerPort.ps1
+++ TCPConnectionPerPort.ps1
@@ -1,2 +1,1 @@
-Get-NetTCPConnection -remoteport 1433 | group State, OwningProcess | Select Count,RemotePort,@{L='State';E={$_.Name.Split(",")[0]}}, @{L='Process';E={Get-Process -Id ($_.Nam
-e.Split(",")[1].Trim()) | Select -Expand ProcessName}}
+Get-NetTCPConnection -remoteport 1433 | group State, OwningProcess | Select Count,RemotePort,@{L='State';E={$_.Name.Split(",")[0]}}, @{L='Process';E={Get-Process -Id ($_.Name.Split(",")[1].Trim()) | Select -Expand ProcessName}}
